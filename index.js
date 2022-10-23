42a8d486fb6087a3185a49f49468354d6f6883ea
Carlos Rojas <caranroja@outlook.com>
2022-10-22 22:57:33 -0500

    Se agrega el middleware de cors para permitir peticiones desde el front

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
 const express = require("express");
 const { getConnection } = require("./db/db-connection-mongo");
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/cliente', require('./router/cliente'));
 app.use('/tipoproyecto', require('./router/tipoProyecto'));
